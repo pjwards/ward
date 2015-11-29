58228c8893bc482d429316cd9b297bb53d573b80
Jisung <cbajs20@gmail.com>
2015-11-29 16:30:57 +0900

    update spam algorithm

--- www/analysis/spam_app.py
+++ www/analysis/spam_app.py
@@ -21,7 +21,7 @@ class Spam:
         :param string_data: string data that have to be analyzed
         :return: analyzed data list
         """
-        spam_db = SpamWordList.objects.all()     # all spamlist data loads in list
+        spam_db = SpamWordList.objects.filter(group=self.group)     # all spamlist data loads in list
         data_set = [sp.word for sp in spam_db]
         twitter_posmore = self.analyzer.analyzer_twitter(string_data, 'posmore')
         kkma_nouns = self.analyzer.analyzer_kkma(string_data, 'nouns')
@@ -38,7 +38,7 @@ class Spam:
         Add words or update count of words in database
         :param analyzed_words: analyzed data set
         """
-        spam_db = SpamWordList.objects.all()
+        spam_db = SpamWordList.objects.filter(group=self.group)
         words_s = sorted(analyzed_words)
         temp = []
         for i in words_s:
@@ -50,7 +50,7 @@ class Spam:
 
         for i in words_s:
             if i[0] not in temp:
-                new_word = SpamWordList(word=i[0])
+                new_word = SpamWordList(word=i[0], group=self.group)
                 new_word.save()
 
     def init_db(self):
@@ -59,7 +59,7 @@ class Spam:
         """
         data_set = [line.strip() for line in open("analysis/texts/spam.txt", 'r')]
         for i in data_set:
-            store_data = SpamWordList(word=i)
+            store_data = SpamWordList(word=i, group=self.group)
             store_data.save()
 
     def delete_words(self, data_list):
@@ -68,41 +68,61 @@ class Spam:
         :param data_list: list of words which are wanted to except from standard word
         """
         for i in data_list:
-            delete_data = SpamWordList.objects.get(word=i)
-            delete_data.status = 'deleted'
-            delete_data.save()
+            try:
+                delete_data = SpamWordList.objects.get(word=i, group=self.group)
+                delete_data.status = 'deleted'
+                delete_data.save()
+            except Exception as expt:
+                print("error occurred:"+expt)
 
     def update_standard_word(self):
         """
         update spam words in database and this procedure is based on spam.txt
         """
         data_set = [line.strip() for line in open("analysis/texts/spam.txt", 'r')]
-        db_all = SpamWordList.objects.all()
+        db_all = SpamWordList.objects.filter(group=self.group)
         standard = [a.word for a in db_all]
         for j in data_set:
             if j not in standard:
-                store_data = SpamWordList(word=j)
+                store_data = SpamWordList(word=j, group=self.group)
                 store_data.save()
 
-    def improve_analysis_level(self):
+    def improve_analysis_level(self):           # plus url
+        """
+        Add words that duplicated in deleted state texts
+        """
         spam_db = SpamContentList.objects.filter(status='deleted')
-        text_list = [sp.text for sp in spam_db]
-        for i in text_list:
-            spam_db2 = SpamWordList.objects.all()
-            db_data = [sp.word for sp in spam_db2]
-            word_list = core.analyze_text(i, db_data)
-            for j in word_list:
-                if j[1] == 10:
+        db_text_list = [sp.text for sp in spam_db]
+        words_list = []
+        for i in db_text_list:
+            twitter_posmore = self.analyzer.analyzer_twitter(i, 'posmore')
+            kkma_nouns = self.analyzer.analyzer_kkma(i, 'nouns')
+            twitter_word_list = []
+            for j in twitter_posmore:
+                twitter_word_list.append(j[0])
+            merged_list = kkma_nouns + twitter_word_list
+            words_list += merged_list
+
+        spam_db2 = SpamWordList.objects.filter(group=self.group)
+        db_words = [sp.word for sp in spam_db2]
+        duped_words = core.analyze_text(words_list, db_words)
+        for j in duped_words:
+            if j[1] == 10:
+                try:
                     word = SpamWordList.objects.get(word=j[0], group=self.group)
                     word.count += 10
                     word.save()
-                else:
-                    word = SpamWordList(word=j[0], count=1, group=self.group)
-                    word.save()
+                except Exception as expt:
+                    print("error occurred:"+expt)
+            else:
+                word = SpamWordList(word=j[0], group=self.group)
+                word.save()
 
         #urls = core.get_url_from_string(string_data)
         #url_list = core.url_duplication_check(data_set,urls)
-    ##warning sign for same spam content
+
+    ##alarm about spam that deleted in other group
+    ##remmadation function
 
 
 def run_app():      # test-only method
