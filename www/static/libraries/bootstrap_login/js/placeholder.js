5c331464b49dafcfbc73294eefdab4fb21a273e7
Donghyun Seo <egaoneko@naver.com>
2015-12-30 14:54:03 +0900

    Edit comments

--- www/static/libraries/bootstrap_login/js/placeholder.js
+++ www/static/libraries/bootstrap_login/js/placeholder.js
@@ -1,6 +1,3 @@
-/**
- * Created by donghyun on 11/25/15.
- */
 $(document).ready(function () {
     $(".form-username").val("Username...");
     $(".form-email").val("Email...");
--- www/test.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Donghyun Seo"
-__copyright__ = "Copyright ⓒ 2015, All rights reserved."
-__email__ = "egaoneko@naver.com"
-
-
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b"Hello World"] # python3
